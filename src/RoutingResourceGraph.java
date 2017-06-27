5ba3fd755efd434dbdbdb29e502ec9cca109e777
yang0323weepy <yang0323weepy@gmail.com>
2017-06-27 16:58:15 -0400

    add dialog window

--- src/RoutingResourceGraph.java
+++ src/RoutingResourceGraph.java
@@ -1,5 +1,8 @@
 
 import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 import javax.swing.*;
 
 /*
@@ -35,14 +38,54 @@ public class RoutingResourceGraph extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        showPro = new javax.swing.JDialog();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        prosperity = new javax.swing.JTextArea();
+        exitD = new javax.swing.JButton();
         graphPanel = new DrawGraph(ex,squares);
         route = new javax.swing.JButton();
         reroute = new javax.swing.JButton();
         showState = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         about = new javax.swing.JMenuItem();
         exit = new javax.swing.JMenuItem();
+
+        showPro.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+
+        prosperity.setColumns(20);
+        prosperity.setRows(5);
+        jScrollPane1.setViewportView(prosperity);
+
+        exitD.setText("OK");
+
+        javax.swing.GroupLayout showProLayout = new javax.swing.GroupLayout(showPro.getContentPane());
+        showPro.getContentPane().setLayout(showProLayout);
+        showProLayout.setHorizontalGroup(
+            showProLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(showProLayout.createSequentialGroup()
+                .addGroup(showProLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(showProLayout.createSequentialGroup()
+                        .addGap(190, 190, 190)
+                        .addComponent(exitD, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(showProLayout.createSequentialGroup()
+                        .addGap(96, 96, 96)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 313, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(87, Short.MAX_VALUE))
+        );
+        showProLayout.setVerticalGroup(
+            showProLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(showProLayout.createSequentialGroup()
+                .addContainerGap(36, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 226, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(exitD)
+                .addGap(35, 35, 35))
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
@@ -62,6 +105,25 @@ public class RoutingResourceGraph extends javax.swing.JFrame {
                 rerouteActionPerformed(evt);
             }
         });
+
+        showState.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        showState.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                showStateMouseClicked(evt);
+            }
+        });
+
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Yellow: wire not in use");
+
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Red: Sink node");
+
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("Green: wire in use");
+
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel4.setText("Blue: Source node");
 
         jMenu1.setText("Menu");
 
@@ -86,34 +148,47 @@ public class RoutingResourceGraph extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(showState, javax.swing.GroupLayout.PREFERRED_SIZE, 237, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(26, 26, 26))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(106, 106, 106)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addGap(42, 42, 42)
+                        .addComponent(showState, javax.swing.GroupLayout.PREFERRED_SIZE, 237, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(128, 128, 128)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(reroute)
-                            .addComponent(route, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(106, 106, 106)))
+                            .addComponent(route, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(97, 97, 97)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGap(158, 158, 158)
                 .addComponent(graphPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 455, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(79, Short.MAX_VALUE))
+                .addContainerGap(27, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap(40, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(showState, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(65, 65, 65)
-                        .addComponent(route)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(reroute)
-                        .addGap(118, 118, 118))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(graphPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 339, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(23, 23, 23))))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(55, 55, 55)
+                .addComponent(showState, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(route)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(reroute)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(33, Short.MAX_VALUE)
+                .addComponent(graphPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 339, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(30, 30, 30))
         );
 
         pack();
@@ -121,8 +196,7 @@ public class RoutingResourceGraph extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void routeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_routeActionPerformed
-
-        // TODO add your handling code here:
+    // TODO add your handling code here:
         System.out.println("===============================");
         ex.runIteration();
         if (!ex.getGraph().testCong()) {  
@@ -148,6 +222,11 @@ public class RoutingResourceGraph extends javax.swing.JFrame {
         // TODO add your handling code here:
         System.exit(0);
     }//GEN-LAST:event_exitActionPerformed
+
+    private void showStateMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_showStateMouseClicked
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_showStateMouseClicked
 
     /**
      * @param args the command line arguments
@@ -203,9 +282,24 @@ public class RoutingResourceGraph extends javax.swing.JFrame {
                 squares[i][j].setBackground(Color.red);
                 squares[i][j].setOpaque(true);
                 }
+                squares[i][j].addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    showPro.setVisible(true);
+                }
+            });
                 graphPanel.add(squares[i][j]);
             }  
         } 
+    }
+    
+    public void addMouse(){
+       ArrayList<RoutingGraph<Integer>.Node<Integer>> sources = new ArrayList<RoutingGraph<Integer>.Node<Integer>>();
+        for (int i = 0; i < ex.getGraph().getGraph().size(); i++) {
+            if (ex.getGraph().getGraph().get(i).getState() == 0) {
+                sources.add(ex.getGraph().getGraph().get(i));
+            }
+        }
     }
 
     
@@ -214,11 +308,19 @@ public class RoutingResourceGraph extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenuItem about;
     private javax.swing.JMenuItem exit;
+    private javax.swing.JButton exitD;
     private javax.swing.JPanel graphPanel;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea prosperity;
     private javax.swing.JButton reroute;
     private javax.swing.JButton route;
+    private javax.swing.JDialog showPro;
     private javax.swing.JLabel showState;
     // End of variables declaration//GEN-END:variables
 }
--- src/RunGraph.java
+++ src/RunGraph.java
@@ -15,7 +15,6 @@ import java.util.*;
 public class RunGraph {
      public static void main(String[] args) {
        RunGraph ex = new RunGraph();
-       ex.run();
          
     }
 
@@ -24,11 +23,12 @@ public class RunGraph {
         graph = new RoutingGraph<Integer>();
                 System.out.println(graph);
         for (int j = 0; j < 16; j++) {
-            if (j < 8) {
+            if (j < 4) {
+                graph.AddNode(j);
+                
+            } else if (j >= 4 && j < 12) {
                 graph.AddNode(j);
                 graph.getGraph().get(j).setState(1);
-            } else if (j >= 8 && j < 12) {
-                graph.AddNode(j);               
             } else {
                 graph.AddNode(j);
                 graph.getGraph().get(j).setState(2);
@@ -40,36 +40,7 @@ public class RunGraph {
         }
         find_shortest_path();
      }
-    /**
-     * increase the size of graph from 5 to 1000
-     * add node to the graph,add edge, and find shortest path 
-     */
-
-    //method to run the whole program
-    public void run() {
-        graph = new RoutingGraph<Integer>();
-        for (int j = 0; j < 16; j++) {
-            if (j < 8) {
-                graph.AddNode(j);   
-                graph.getGraph().get(j).setState(1);
-            } else if (j >= 8 && j < 12) {
-                graph.AddNode(j);
-            } else {
-                graph.AddNode(j);
-                graph.getGraph().get(j).setState(2);
-            }
-        }
-        add_edge();
-        for(int i = 0; i < graph.getGraph().size(); i++){
-            System.out.println("cost " + graph.getGraph().get(i).getCost());
-        }
-        find_shortest_path();
-        while(!testCong(graph)){
-            System.out.println("===============================");
-            runIteration();
-        }
-    }
-
+  
     /**
      * the method to add edge between two random city, the weight is randomized between 1 and 10
      * 
@@ -77,57 +48,42 @@ public class RunGraph {
     //the method to add edge (the node and the weight) between two random city in the graph
     public void add_edge(){
         //generate edges and add them into the graph
-        graph.AddEdge(0, 2, 0.1);
-        graph.AddEdge(0, 4, 0.5);
-        graph.AddEdge(4, 3, 0.3);
-        graph.AddEdge(3, 4, 2.0);
-        graph.AddEdge(5, 7, 0.5);
-        graph.AddEdge(1, 7, 0.5);
-        graph.AddEdge(5, 4, 0.5);
-        graph.AddEdge(2, 6, 0.1);
-        graph.AddEdge(6, 2, 0.1);
-        graph.AddEdge(3, 0, 0.2);
-        graph.AddEdge(3, 7, 0.2);
-        graph.AddEdge(7, 0,0.5);
-       graph.AddEdge(8,0,graph.getGraph().get(0).getCost());  
-       graph.AddEdge(8,1,graph.getGraph().get(1).getCost());  
-       graph.AddEdge(9,2,graph.getGraph().get(2).getCost());  
-       graph.AddEdge(9,3,graph.getGraph().get(3).getCost());  
-       graph.AddEdge(10,4,graph.getGraph().get(4).getCost());  
-       graph.AddEdge(10,5,graph.getGraph().get(5).getCost());  
-       graph.AddEdge(11,6,graph.getGraph().get(6).getCost());  
-       graph.AddEdge(11,7,graph.getGraph().get(7).getCost());   
-       graph.AddEdge(0,12,graph.getGraph().get(0).getCost());   
-       graph.AddEdge(1,12,graph.getGraph().get(1).getCost());   
-       graph.AddEdge(2,13,graph.getGraph().get(2).getCost());
-       graph.AddEdge(3,13,graph.getGraph().get(3).getCost());  
-       graph.AddEdge(4,14,graph.getGraph().get(4).getCost());  
-       graph.AddEdge(5,14,graph.getGraph().get(5).getCost());  
-       graph.AddEdge(6,15,graph.getGraph().get(6).getCost());  
-       graph.AddEdge(7,15,graph.getGraph().get(7).getCost()); 
-       graph.AddEdge(0,13,graph.getGraph().get(0).getCost());   
-       graph.AddEdge(1,14,graph.getGraph().get(1).getCost());   
-       graph.AddEdge(2,15,graph.getGraph().get(2).getCost());
-       graph.AddEdge(3,12,graph.getGraph().get(3).getCost());  
-       graph.AddEdge(7,13,graph.getGraph().get(7).getCost());
-       graph.AddEdge(0,15,graph.getGraph().get(0).getCost());    
-       graph.AddEdge(3,15,graph.getGraph().get(3).getCost());  
-       graph.AddEdge(7,12,graph.getGraph().get(7).getCost());  
-    }
-    /**
-     * the method to decide whether discard the graph
-     * if not all nodes can be reached, the method return false
-     * 
-     * @return     true or false
-     */
-    //the method to decide whether discard the graph
-    public boolean discard_graph(){
-        for(int i = 0; i < graph.getGraph().size(); i++){
-            if(graph.getGraph().get(i).getEdge().isEmpty()){
-                return false;
-            }
-        }
-        return true;
+        graph.AddEdge(4, 6, 0.1);
+        graph.AddEdge(4, 8, 0.5);
+        graph.AddEdge(8, 7, 0.3);
+        graph.AddEdge(7, 8, 2.0);
+        graph.AddEdge(9, 11, 0.5);
+        graph.AddEdge(5, 11, 0.5);
+        graph.AddEdge(9, 8, 0.5);
+        graph.AddEdge(6, 10, 0.1);
+        graph.AddEdge(10, 6, 0.1);
+        graph.AddEdge(7, 4, 0.2);
+        graph.AddEdge(7, 11, 0.2);
+        graph.AddEdge(11, 4,0.5);
+       graph.AddEdge(0,4,graph.getGraph().get(0).getCost());  
+       graph.AddEdge(0,5,graph.getGraph().get(1).getCost());  
+       graph.AddEdge(1,6,graph.getGraph().get(2).getCost());  
+       graph.AddEdge(1,7,graph.getGraph().get(3).getCost());  
+       graph.AddEdge(2,8,graph.getGraph().get(4).getCost());  
+       graph.AddEdge(2,9,graph.getGraph().get(5).getCost());  
+       graph.AddEdge(3,10,graph.getGraph().get(6).getCost());  
+       graph.AddEdge(3,11,graph.getGraph().get(7).getCost());   
+       graph.AddEdge(4,12,graph.getGraph().get(0).getCost());   
+       graph.AddEdge(5,12,graph.getGraph().get(1).getCost());   
+       graph.AddEdge(6,13,graph.getGraph().get(2).getCost());
+       graph.AddEdge(7,13,graph.getGraph().get(3).getCost());  
+       graph.AddEdge(8,14,graph.getGraph().get(4).getCost());  
+       graph.AddEdge(9,14,graph.getGraph().get(5).getCost());  
+       graph.AddEdge(10,15,graph.getGraph().get(6).getCost());  
+       graph.AddEdge(11,15,graph.getGraph().get(7).getCost()); 
+       graph.AddEdge(4,13,graph.getGraph().get(0).getCost());   
+       graph.AddEdge(5,14,graph.getGraph().get(1).getCost());   
+       graph.AddEdge(6,15,graph.getGraph().get(2).getCost());
+       graph.AddEdge(7,12,graph.getGraph().get(3).getCost());  
+       graph.AddEdge(11,13,graph.getGraph().get(7).getCost());
+       graph.AddEdge(4,15,graph.getGraph().get(0).getCost());    
+       graph.AddEdge(7,15,graph.getGraph().get(3).getCost());  
+       graph.AddEdge(11,12,graph.getGraph().get(7).getCost());  
     }
 
     /**
